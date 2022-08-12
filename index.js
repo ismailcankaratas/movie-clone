ed6d94a735b90f7a42536d836fb6b40c5dc2d5e4
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2022-08-12 19:03:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const mobilMenu = document.getElementById("mobil-menu");
+
+function menuOpen() {
+    mobilMenu.style.display = "block";
+}
+
+function menuClose() {
+    mobilMenu.style.display = "none";
+}
