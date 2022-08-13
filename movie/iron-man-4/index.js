42e8730bdb91ea8fc39b0db23dff72658365582a
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2022-08-14 01:21:36 +0300

    Added iframe options.

--- movie/iron-man-4/index.js
+++ movie/iron-man-4/index.js
@@ -3,20 +3,30 @@ const videoSourceMenu = document.getElementById("video-source-menu");
 const shareDropdown = document.getElementById("shareDropdown");
 const dropdownShare = document.getElementById("dropdown-share");
 const commentSortDropdown = document.getElementById("comment-sort-dropdown");
+const iframeEmbed = document.getElementById("iframe-embed");
 
 function videoSourceToggle() {
     toggle(videoSourceMenu, "block")
 }
 
 function vieoSourceSelect(value) {
-    const { name, embed } = value;
-    if (name && embed) {
+    const { name, iframe } = value;
+    if (name && iframe) {
         console.log(value);
         videoSource.innerHTML =
             `
         Source : <span>${name}</span>
         `;
         videoSourceMenu.style.display = "none";
+        iframeEmbed.innerHTML =
+            `
+        <iframe
+        src="${iframe}"
+        data-src="${iframe}"
+        frameborder="0" scrolling="no" allow="autoplay; encrypted-media"
+        allowfullscreen="">
+        </iframe>
+        `;
     }
 }
 
